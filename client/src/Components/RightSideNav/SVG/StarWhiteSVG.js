4e6a7e1416a471b01b575549cca29e3c5a706917
michikogo <37466954+michikogo@users.noreply.github.com>
2021-08-10 00:59:11 -0400

    star svg

---
+++ client/src/Components/RightSideNav/SVG/StarWhiteSVG.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const StarWhiteSVG = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 512 512"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M512 199.387L334.8 174.148L256 17.647L177.2 174.148L0 199.387L128.256 321.928L98.084 494.353L256 412.978L413.916 494.353L383.744 321.928L512 199.387ZM256 367.909L151.456 421.782L171.409 307.748L87.33 227.416L203.57 210.861L256 106.732L308.43 210.861L424.669 227.416L340.59 307.748L360.543 421.782L256 367.909Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
+
+export default StarWhiteSVG;
