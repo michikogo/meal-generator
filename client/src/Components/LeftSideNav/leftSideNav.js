4110305e8c3a4f51212cdfac6f87874b826243ce
michikogo <37466954+michikogo@users.noreply.github.com>
2021-08-09 22:29:46 -0400

    placed SVG

--- client/src/Components/LeftSideNav/leftSideNav.js
+++ client/src/Components/LeftSideNav/leftSideNav.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { Row, Col } from "antd";
+
+import ExploreSVG from "./SVG/ExploreSVG";
+import HomeSVG from "./SVG/HomeSVG";
+import BookmarkSVG from "./SVG/BookmarkSVG";
+import NotificationsSVG from "./SVG/NotificationsSVG";
+import ProfileSVG from "./SVG/ProfileSVG";
+import MessagesSVG from "./SVG/MessagesSVG";
 
 const LeftSideNav = () => {
+  const menu = [
+    { name: "Home", icon: <HomeSVG /> },
+    { name: "Explore", icon: <ExploreSVG /> },
+    { name: "Notifications", icon: <NotificationsSVG /> },
+    { name: "Messages", icon: <MessagesSVG /> },
+    { name: "Bookmarks", icon: <BookmarkSVG /> },
+    { name: "Profile", icon: <ProfileSVG /> },
+  ];
   return (
     <div>
-      Home, explore, notifications, messages, bookmarks, profile, addmovie
+      {menu.map((items, index) => (
+        <Row key={index} gutter={(12, 12)}>
+          <Col>{items.icon}</Col>
+          <Col>
+            <p style={{ fontSize: "x-large" }}>{items.name}</p>
+          </Col>
+        </Row>
+      ))}
     </div>
   );
 };
